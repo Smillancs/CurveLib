0620ba9c4f5cbd664943c9107298414ae025bf7a
Smillancs <smillancs@gmail.com>
2017-05-02 08:32:04 +0200

    Added new BezierCurve examples for 2D

--- CurveLib/Examples.cpp
+++ CurveLib/Examples.cpp
@@ -28,27 +28,44 @@ size_t ExampleHandler::size()
 
 void ExampleHandler::generate()
 {
-	examples.push_back(new Line(glm::dvec3(0,0,0), glm::dvec3(1,2,3)));
-	examples.push_back(new Circle(glm::dvec3(1,2,3), 3.0));
-	examples.push_back(new Spiral(glm::dvec3(0, 0, 0), 5));
+//	examples.push_back(new Line(glm::dvec3(0,0,0), glm::dvec3(1,2,3)));
+//	examples.push_back(new Circle(glm::dvec3(1,2,3), 3.0));
+//	examples.push_back(new Spiral(glm::dvec3(0, 0, 0), 5));
 
 	std::vector<glm::vec3> cps;
-	cps.push_back(glm::vec3(0, 0, 4));
-	cps.push_back(glm::vec3(0, 4, 0));
-	cps.push_back(glm::vec3(-8, 2, 0));
-	cps.push_back(glm::vec3(0, 4, -2));
-	cps.push_back(glm::vec3(-2, -8, 4));
-	cps.push_back(glm::vec3(0, 10, 0));
+	cps.push_back(glm::vec3(2,8,1));
+	cps.push_back(glm::vec3(4,0,1));
+	cps.push_back(glm::vec3(0,8,1));
+	cps.push_back(glm::vec3(-5,0,1));
+	cps.push_back(glm::vec3(1,-4,1));
 	
 	examples.push_back(new BezierCurve(cps));
-	
+
 	std::vector<glm::vec3> cps2;
-	cps2.push_back(glm::vec3(1,0,0));
-	cps2.push_back(glm::vec3(2,3,1));
-	cps2.push_back(glm::vec3(-1,1,2));
-	cps2.push_back(glm::vec3(0,-2,0));
-	cps2.push_back(glm::vec3(0,-2,0));
+	cps2.push_back(glm::vec3(1, 0, 1));
+	cps2.push_back(glm::vec3(2, 3, 1));
+	cps2.push_back(glm::vec3(-1, 1, 1));
+	cps2.push_back(glm::vec3(0, -2, 1));
+	cps2.push_back(glm::vec3(0, -2, 1));
 
 	examples.push_back(new BezierCurve(cps2));
+
+	std::vector<glm::vec3> cps3;
+	cps3.push_back(glm::vec3(-1, -1, 1));
+	cps3.push_back(glm::vec3(-1, 1, 1));
+	cps3.push_back(glm::vec3(1, 1, 1));
+	cps3.push_back(glm::vec3(1, -1, 1));
+	cps3.push_back(glm::vec3(-1, -1, 1));
+
+	examples.push_back(new BezierCurve(cps3));
+
+	std::vector<glm::vec3> cps4;
+	cps4.push_back(glm::vec3(-4, -4, 1));
+	cps4.push_back(glm::vec3(-4, 4, 1));
+	cps4.push_back(glm::vec3(4, 4, 1));
+	cps4.push_back(glm::vec3(4, -4, 1));
+	cps4.push_back(glm::vec3(8, -4, 1));
+
+	examples.push_back(new BezierCurve(cps4));
 	ready = true;
 }
